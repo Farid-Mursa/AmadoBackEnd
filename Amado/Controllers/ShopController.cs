a3cd4ec76dea9724168f725f868c43bd58fe3743
Farid-Mursa <faridfmur@code.edu.az>
2024-01-16 21:48:37 +0400

    LastSomeAdds

--- Amado/Controllers/ShopController.cs
+++ Amado/Controllers/ShopController.cs
@@ -17,14 +17,17 @@ namespace Amado.Controllers
         {
             _context = context;
         }
-        public async Task<IActionResult> Index(int page, string order = "desc")
+        public async Task<IActionResult> Index(int page = 1, string order = "desc", int viewTake = 2)
         {
+            ViewBag.ActivePage = "Shop";
+
             if (page <= 0) page = 1;
 
-            int productsPerPage = 3;
+            int productsPerPage = viewTake;
+
             var productCount = await _context.Products.CountAsync();
 
-            int totalPageCount = (int)Math.Ceiling(((decimal)productCount / productsPerPage));
+
 
             var productsQuery = order switch
             {
@@ -33,11 +36,20 @@ namespace Amado.Controllers
                 _ => _context.Products.OrderByDescending(x => x.Id)
             };
 
-            var pagedProducts = await productsQuery
-                .Skip((page - 1) * productsPerPage)
-                .Take(productsPerPage)
+            var productViewTake = viewTake switch
+            {
+                2 => productsQuery.Skip((page - 1) * productsPerPage).Take(2),
+                4 => productsQuery.Skip((page - 1) * productsPerPage).Take(4),
+                6 => productsQuery.Skip((page - 1) * productsPerPage).Take(6),
+                _ => productsQuery.Skip((page - 1) * productsPerPage).Take(productsPerPage)
+            };
+
+            int totalPageCount = (int)Math.Ceiling(((decimal)productCount / productsPerPage));
+            var pagedProducts = await productViewTake
+                .OrderByDescending(x => x.Id)
                 .Include(p => p.ProductImages).ThenInclude(pi => pi.Image)
                 .ToListAsync();
+
 
             ViewBag.Order = order;
             ViewBag.Categories = _context.Category.ToList();
@@ -46,15 +58,51 @@ namespace Amado.Controllers
 
             var model = new IndexVM
             {
-                Products = pagedProducts
-                .Skip((page - 1) * productsPerPage)
-                .Take(3)
-                .ToList(),
+                Products = pagedProducts,
                 TotalPageCount = totalPageCount,
                 CurrentPage = page,
             };
 
             return View(model);
+        }
+
+
+        public async Task<IActionResult> Filter(int page = 1, string order = "desc", int viewTake = 2)
+        {
+            if (page <= 0) page = 1;
+
+            int productsPerPage = viewTake;
+            var productCount = _context.Products.Count();
+
+            int totalPageCount = (int)Math.Ceiling(((decimal)productCount / productsPerPage));
+
+            var products = order switch
+            {
+                "desc" => _context.Products.OrderByDescending(x => x.Id),
+                "asc" => _context.Products.OrderBy(x => x.Id),
+                _ => _context.Products.OrderByDescending(x => x.Id)
+            };
+
+            var productViewTake = viewTake switch
+            {
+                2 => products.Skip((page - 1) * productsPerPage).Take(2),
+                4 => products.Skip((page - 1) * productsPerPage).Take(4),
+                6 => products.Skip((page - 1) * productsPerPage).Take(6),
+                _ => products.Skip((page - 1) * productsPerPage).Take(productsPerPage)
+            };
+
+            var model = new IndexVM
+            {
+                Products = await productViewTake
+
+                .OrderByDescending(p => p.Id)
+                .Include(p => p.ProductImages).ThenInclude(pi => pi.Image)
+                .ToListAsync(),
+                TotalPageCount = totalPageCount,
+                CurrentPage = page,
+            };
+
+            return PartialView("_ShopPartial", model);
         }
 
         public IActionResult Detail(int? id)
@@ -74,7 +122,6 @@ namespace Amado.Controllers
 
             return View(model);
         }
-
 
         public IActionResult AddToBasket(int? id)
         {
@@ -139,6 +186,7 @@ namespace Amado.Controllers
 
             return RedirectToAction("Index", "Cart");
         }
+
         public IActionResult Search(string? input)
         {
             var products = input == null ? new List<Product>()
@@ -149,34 +197,6 @@ namespace Amado.Controllers
             return ViewComponent("SearchResult", products);
         }
 
-        public IActionResult Filter(int page, string order = "desc")
-        {
-            if (page <= 0) page = 1;
-
-            int productsPerPage = 3;
-            var productCount = _context.Products.Count();
-
-            int totalPageCount = (int)Math.Ceiling(((decimal)productCount / productsPerPage));
-
-            var products = order switch
-            {
-                "desc" => _context.Products.OrderByDescending(x => x.Id),
-                "asc" => _context.Products.OrderBy(x => x.Id),
-                _ => _context.Products.OrderByDescending(x => x.Id)
-            };
-
-            var model = new IndexVM
-            {
-                Products = products
-                .Skip((page - 1) * productsPerPage)
-                .Take(productsPerPage)
-                .ToList(),
-                TotalPageCount = totalPageCount,
-                CurrentPage = page
-            };
-
-            return PartialView("_ShopPartial", model);
-        }
 
         public IActionResult Sorted(int titleid, int brandid, int colorid, int page = 1)
         {
@@ -202,7 +222,7 @@ namespace Amado.Controllers
                    .Where(x => (titleid == 0 || x.CategoryId == titleid)
                             && (brandid == 0 || x.BrandId == brandid)
                             && (colorid == 0 || x.ProductColors.FirstOrDefault().ColorId == colorid))
-                   .OrderBy(x => x.Id)
+                   .OrderByDescending(x => x.Id)
                    .Skip((page - 1) * 8)
                    .ToList();
             var model = new IndexVM
